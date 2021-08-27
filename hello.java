04fce4804bb425771bfb026bb50a154a57e0ecd1
selcukozen <selcukozen82@gmail.com>
2021-08-27 23:52:05 +0300

    elf

---
+++ hello.java
@@ -0,0 +1,15 @@
+/**
+ * hello
+ */
+public class hello {
+
+  public static void main(String[] args) {
+    //System.out.print("Hello Wrold");
+    //System.out.println("Merhaba java");
+    //falan filan.
+
+    int number = 5;
+
+    System.out.print(number);
+  }
+}
